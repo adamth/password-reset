7f5be4683f82256c6b7bd76d64f3b244e02646f0
adamth <adam@adamth.com>
2017-05-18 15:40:42 +1000

    Set up user schema and passport strategy

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var mongoose = require('mongoose');
+var nodemailer = require('nodemailer');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var bcrypt = require('bcrypt-nodejs');
+var async = require('async');
+var crypto = require('crypto');
 
+mongoose.connect('localhost');
 var app = express();
 
 // Middleware
@@ -14,7 +23,59 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+app.use(session({ secret: 'session secret key' }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+var userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date
+});
+
+userSchema.pre('save', function(next) {
+  const user = this;
+  const SALT_FACTOR = 5;
+
+  if (!user.isModified('password')) return next();
+
+  bcrypt.genSalt(SALT_FACTOR,(err, salt) => {
+    if(err) return next(err);
+
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if(err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, (err, result) => {
+    if(err) return cb(err);
+    cb(null, result);
+  });
+};
+
+var User = mongoose.model('User', userSchema);
+
+passport.use(new LocalStrategy((username, password, done) => {
+  User.findOne({ username }).then((user) => {
+    if (!user){
+      return done(null, false, {message: 'REMOVE THIS: Incrrect username'});
+    }
+    user.comparePassword(password, (err, isMatch) => {
+      if(isMatch) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: 'Incorrect password' });
+      }
+    })
+  }).catch((err) => {
+    return done(err);
+  });
+}));
 
 // Routes
 app.get('/', function(req, res) {
